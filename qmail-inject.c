ncroiset/notqmail

commit 56e7c4a74087d47889355c2c55352cc4d0482020

qmail-inject: do not parse header recipients if "-a" is given
qmail-inject can parse the message header to determine the recipients of a mail.
You can also pass it a list of recipients on command line so it will ignore the
recipients stated in the header. This is e.g. used when you redirect a mail by
a .qmail file. But even if those recipients will not be used the header fields
will still be parsed and a bounce is generated on failure.

If a mail is injected from network with the broken header field

  To: foo: bar, baz

then this would be delivered fine as the recipient is given in the SMTP
transmission. If it could not be directly delivered but is forwarded to another
user by a .qmail file or a virtualdomain alias then this mail is passed through
"qmail-inject -a newrecipient". Nevertheless the header fields are parsed, the
error is raised and the message is bounced. This leads to ugly bounces with the
message "qmail-inject: fatal: unable to parse this line:". Since the message is
of course spam the bounce will bounce again and annoy the postmaster. This patch
changes qmail-inject to totally ignore the lines with recipients (To:, Cc:, Bcc)
if the recipients were passed as arguments. In this case the mail will be routed
to the original recipient and can there be handled.

qmail-inject.c
@@ -358,11 +358,14 @@ stralloc *h;
  
   switch(htype) {
     case H_TO: case H_CC:
-      rw = rwtocc; break;
+      if (flagrh) rw = rwtocc;
+      break;
     case H_BCC: case H_APPARENTLYTO:
-      rw = rwhr; break;
+      if (flagrh) rw = rwhr;
+      break;
     case H_R_TO: case H_R_CC: case H_R_BCC:
-      rw = rwhrr; break;
+      if (flagrh) rw = rwhrr;
+      break;
     case H_RETURNPATH:
       rw = rwreturn; break;
     case H_SENDER: case H_FROM: case H_REPLYTO:

commit e557386a05620cafb95fa6ef096f841398d7703d

qmail-inject: fix header parse regression
When attempting to catch incorrectly formatted headers like "To: foo: bar, baz"
to avoid errors when injecting bounces the host expansion of headers like
"To: root" was also broken.

Instead of completely ignoring these lines when the recipient list was already
given on the command line using -a option try to process these lines as before,
but allow the parsing of these lines to silently fail. If a broken line is
encountered this now simply does not reformat them to insert the hostname.

Fixes: 56e7c4a74087d47889355c2c55352cc4d0482020

qmail-inject.c
@@ -76,6 +76,14 @@ void doordie(sa,r) stralloc *sa; int r; {
  if (r == 1) return; if (r == -1) die_nomem();
  substdio_putsflush(subfderr,"qmail-inject: fatal: unable to parse this line:\n");
  substdio_putflush(subfderr,sa->s,sa->len); perm(); }
+/* call doordie, but if q is set ignore parse errors (i.e. r == 0) */
+static int doordie_rh(stralloc *sa, int r, int q)
+{
+  if (q && r == 0)
+    return 0;
+  doordie(sa, r);
+  return 1;
+}
 
 GEN_ALLOC_typedef(saa,stralloc,sa,len,a)
 GEN_ALLOC_readyplus(saa,stralloc,sa,len,a,10,saa_readyplus)
@@ -347,6 +355,7 @@ stralloc *h;
 {
   int htype;
   int (*rw)() = 0;
+  int rwmayfail = 0;
  
   htype = hfield_known(h->s,h->len);
   if (flagdeletefrom) if (htype == H_FROM) return;
@@ -361,13 +370,16 @@ stralloc *h;
  
   switch(htype) {
     case H_TO: case H_CC:
-      if (flagrh) rw = rwtocc;
+      rw = rwtocc;
+      rwmayfail = 1;
       break;
     case H_BCC: case H_APPARENTLYTO:
-      if (flagrh) rw = rwhr;
+      rw = rwhr;
+      rwmayfail = 1;
       break;
     case H_R_TO: case H_R_CC: case H_R_BCC:
-      if (flagrh) rw = rwhrr;
+      rw = rwhrr;
+      rwmayfail = 1;
       break;
     case H_RETURNPATH:
       rw = rwreturn; break;
@@ -378,10 +390,11 @@ stralloc *h;
   }
 
   if (rw) {
-    doordie(h,token822_parse(&hfin,h,&hfbuf));
-    doordie(h,token822_addrlist(&hfrewrite,&hfaddr,&hfin,rw));
-    if (token822_unparse(h,&hfrewrite,LINELEN) != 1)
-      die_nomem();
+    if (doordie_rh(h,token822_parse(&hfin,h,&hfbuf),rwmayfail) &&
+        doordie_rh(h,token822_addrlist(&hfrewrite,&hfaddr,&hfin,rw),rwmayfail)) {
+      if (token822_unparse(h,&hfrewrite,LINELEN) != 1)
+        die_nomem();
+    }
   }
  
   if (htype == H_BCC) return;